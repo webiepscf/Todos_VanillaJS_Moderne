8f64ce00cf5dc02365695be1c9d0a3dae30052f0
pascalllacroix <webiepscf@gmail.com>
2020-06-27 14:02:13 +0200

    Affichage du nombre de tâches non terminées

--- src/js/modules/Todo.js
+++ src/js/modules/Todo.js
@@ -2,13 +2,14 @@ import todoTemplate from './templates/todo';
 
 export default class Todo {
   constructor (data) {
-    this.parent = data.parent;
-    this.id = data.todo.id;
-    this.content = data.todo.content;
+    this.parent    = data.parent;
+    this.id        = data.todo.id;
+    this.content   = data.todo.content;
     this.completed = data.todo.completed;
-    this.template = todoTemplate;
+    this.template  = todoTemplate;
   }
-  render () {
+
+  _replaceInTemplate() {
     // Je remplace des données statiques par les données du Todo
     for (let propriete in this) {
       this.template = this.template.replace('{{'+propriete+'}}', this[propriete]);
@@ -16,7 +17,10 @@ export default class Todo {
     // Si c'est completed
     this.template = this.template.replace('{{isCompletedClass}}', (this.completed === true)?'completed':'');
     this.template = this.template.replace('{{isCompletedChecked}}', (this.completed === true)?'checked="checked"':'');
+  }
 
+  render () {
+    this._replaceInTemplate();
     const newTodo = document.createElement('div');
     newTodo.innerHTML = this.template;
     this.parent.listEl.appendChild(newTodo);
--- src/js/modules/TodoList.js
+++ src/js/modules/TodoList.js
@@ -5,6 +5,7 @@ export default class TodoList {
   constructor (data) {
     this.el = document.querySelector(data.el);
     this.listEl;
+    this.notCompletedNumber;
     this.todos = [];
     this.loadTodos(data.todos);
     this.template = todoListTemplate;
@@ -15,13 +16,57 @@ export default class TodoList {
       this.todos.push(new Todo({parent: this, todo}));
     }
   }
+
+/**
+ * Rendu de la TodoList
+ * @return {[type]} [description]
+ */
   render () {
     this.el.innerHTML = this.template;
-    // L'élément .todo-list existe pour le navigateur
+    // LE DOM de la liste existe pour le navigateur
       this.listEl = this.el.querySelector('.todo-list');
-
+    // Rendu des todos
       for (let todo of this.todos) {
         todo.render();
       }
+
+    // Calcul du nombre de todos not completed
+      this.setNotCompletedNumber();
+    // Activation des éléments interactifs
+      this.activerBtns();
   }
+
+  setNotCompletedNumber () {
+    this.notCompletedNumber = this.todos.filter(function(todo) {
+      return todo.completed === false;
+    }).length;
+    this.el.querySelector('#todo-count').innerText = this.notCompletedNumber;
+  }
+
+/**
+ * Ajout d'un todo
+ */
+  addTodo () {
+    const content = this.el.querySelector('.new-todo').value;
+    const id = this.todos[this.todos.length - 1].id + 1;
+    const newTodo = new Todo({parent: this, todo: {id, content, completed: false}})
+    this.todos.push(newTodo);
+    newTodo.render();
+    this.el.querySelector('.new-todo').value = '';
+    this.setNotCompletedNumber();
+  }
+
+/**
+ * Activation des éléments interactifs de la ToddoList
+ * @return {[type]} [description]
+ */
+  activerBtns () {
+    // Activation de l'input .new-todo
+      this.el.querySelector('.new-todo').onkeyup = (e) => {
+        if (e.keyCode === 13) {
+          this.addTodo();
+        }
+      };
+  }
+
 }
